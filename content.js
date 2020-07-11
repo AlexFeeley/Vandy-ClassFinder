30ff555b846b9a73cfb33a2194f3d8f6728b13ce
Hadley Shapland <hadley.a.shapland@vanderbilt.edu>
2020-07-11 14:04:33 -0500

    Button shows up on YES page (#3)
    * make YES class popup display hardcoded message
    
    * not working lol
    
    * basic button shows

--- content.js
+++ content.js
@@ -1,25 +1,13 @@
-// Testing
-$('th:contains("Died")').parent().hide()
-$('th:contains("death")').parent().hide()
-$('th:contains("Resting place")').parent().hide()
 
-$('td:contains("death")').parent().hide()
 
-$('span:contains("Death")').parent().hide()
+// var rightSection = document.getElementById("rightSection");
+//
+// rightSection.insertAdjacentHTML('beforeend', '<div class="detailHeader">ExtensionSection</div><div class=""detailPanel"><div class="listItem">List fulfilledrequirements here</div></div>');
 
-$('p').not('.infobox p').first().html(function() {
-	return $(this).html().replace('was', 'is')
-})
 
-var hidden = ['died','remains','funeral','death','autopsy','suicide','cremated','interred','mausoleum']
-hidden.forEach(function(word) {
-	$('p:contains('+word+')').hide()
-	$('li:contains('+word+')').hide()
-	$('span:contains('+word+')').hide()	
-})
+var button = document.createElement("button");
+var text = document.createTextNode("Get Updated Class Details");
+button.appendChild(text);
 
-$('p').each(function() {
-    var html = $(this).html()
-    $(this).html(html.replace('was survived by', 'lives with'))
-    $(this).html(html.replace(/(&nbsp;| )– [0-9a-zA-Z, ]+\)/, ')'))
-})
+var pageLocation = document.getElementById("mainContextMenu");
+pageLocation.appendChild(button);
