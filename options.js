3d1494d8c7d0aaa0211a10b4975aed262e1a1889
Hadley Shapland <hadley.a.shapland@vanderbilt.edu>
2020-07-11 11:38:38 -0500

    add options to switch major

---
+++ options.js
@@ -0,0 +1,28 @@
+// Saves options to chrome.storage
+function save_options() {
+    var major = document.getElementById('major').value;
+    chrome.storage.sync.set({
+        major: major
+    }, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    // Use default value color = 'red' and likesColor = true.
+    chrome.storage.sync.get({
+        major: 'computer-science'
+    }, function(items) {
+        document.getElementById('major').value = items.major;
+    });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
